c1ef22e379bab07ae043b3a2a75d17c90ead5069
ojasskapre <ojasskapre@gmail.com>
2024-03-20 21:29:21 -0700

    fixed signature matching

--- github-push-webhook/src/functions/github-push-event-webhook.js
+++ github-push-webhook/src/functions/github-push-event-webhook.js
@@ -3,7 +3,7 @@ const Crypto = require('crypto');
 module.exports = async function (context, req) {
   context.log('JavaScript HTTP trigger function processed a request.');
 
-  const hmac = Crypto.createHmac('sha1', '<default key>');
+  const hmac = Crypto.createHmac('sha1', 'mysecret');
   const signature = hmac.update(JSON.stringify(req.body)).digest('hex');
   const shaSignature = `sha1=${signature}`;
   const gitHubSignature = req.headers['x-hub-signature'];
