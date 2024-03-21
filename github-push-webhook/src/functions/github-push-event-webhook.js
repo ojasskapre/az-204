476e8c8eb75f137a8e1dcf47e2ecae7acb9d644b
ojasskapre <ojasskapre@gmail.com>
2024-03-20 20:47:59 -0700

    listen for events only from github

--- github-push-webhook/src/functions/github-push-event-webhook.js
+++ github-push-webhook/src/functions/github-push-event-webhook.js
@@ -1,22 +1,36 @@
+const Crypto = require('crypto');
+
 module.exports = async function (context, req) {
   context.log('JavaScript HTTP trigger function processed a request.');
 
-  if (req.body.ref && req.body.ref.startsWith('refs/heads/')) {
-    const repoName = req.body.repository.full_name;
-    const filesChanged = req.body.commits.map((c) => {
-      return {
-        committer_username: c.committer.username,
-        files_added: c.added,
+  const hmac = Crypto.createHmac('sha1', '<default key>');
+  const signature = hmac.update(JSON.stringify(req.body)).digest('hex');
+  const shaSignature = `sha1=${signature}`;
+  const gitHubSignature = req.headers['x-hub-signature'];
+
+  if (!shaSignature.localeCompare(gitHubSignature)) {
+    if (req.body.ref && req.body.ref.startsWith('refs/heads/')) {
+      const repoName = req.body.repository.full_name;
+      const filesChanged = req.body.commits.map((c) => {
+        return {
+          committer_username: c.committer.username,
+          files_added: c.added,
+        };
+      });
+
+      context.res = {
+        body: `${repoName} => ${JSON.stringify(filesChanged)}`,
       };
-    });
-    context.log(repoName);
-    context.res = {
-      body: `${repoName} => ${JSON.stringify(filesChanged)}`,
-    };
+    } else {
+      context.res = {
+        status: 400,
+        body: 'Invalid payload for Push event',
+      };
+    }
   } else {
     context.res = {
-      status: 400,
-      body: 'Invalid payload for Push event',
+      status: 401,
+      body: "Signatures don't match",
     };
   }
 };
